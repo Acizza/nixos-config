9f6ec112f37f67c283e1d51065cb097ee5499920
Acizza <jonathanmce@pm.me>
2018-07-23 02:13:19 -0700

    [anup] Bump version

--- packages/anup.nix
+++ packages/anup.nix
@@ -2,13 +2,13 @@ with import <nixpkgs> {};
 
 rustPlatform.buildRustPackage rec {
     name = "anup-${version}";
-    version = "ce3daa548e63401e6433abf65030c727f8a89b1a";
+    version = "c6e0d13712aa3771fe28b45fbc692eb2ec59024f";
     
     src = fetchFromGitLab {
         owner = "Acizza";
         repo = "anup";
         rev = "${version}";
-        sha256 = "1fgkk81n5nwx8pmaz0px9w4zql33hr8snlpc10qlbldw0x33ihk3";
+        sha256 = "1b41y4fmaly9w2nx80yk6iqfvwfipnsvbr78y535ijyhd6225184";
     };
     
     cargoSha256 = "0x5xj67w5yl38zm339rjdwlvc995yv7z18jwx1prcnpi8i4ssmm7";
