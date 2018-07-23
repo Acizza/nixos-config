58bf83b819898c44c1b0d29693bc8c53e34f0a81
Acizza <jonathanmce@pm.me>
2018-07-23 01:39:06 -0700

    Update bcnotif version

--- packages/bcnotif.nix
+++ packages/bcnotif.nix
@@ -2,16 +2,16 @@ with import <nixpkgs> {};
 
 rustPlatform.buildRustPackage rec {
     name = "bcnotif-${version}";
-    version = "master";
+    version = "4b5f9cb51e52bba898316d1920511b4b0e3502cb";
     
     src = fetchFromGitLab {
         owner = "Acizza";
         repo = "bcnotif";
         rev = "${version}";
-        sha256 = null;
+        sha256 = "17nlvl8kwskc9b6hd9sx3s759whjqr15n0jpjdy551ff59d61z4j";
     };
     
-    cargoSha256 = null;
+    cargoSha256 = "04hpczkmz9fd63yvh34h9xsgq6b8vdb1npc6n5insch6yqrawz2m";
     
     buildInputs = [ openssl dbus pkgconfig ];
     
