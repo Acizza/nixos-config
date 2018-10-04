90a08e9406966b841c5d095d478d753803789636
Acizza <jonathanmce@pm.me>
2018-10-03 21:40:26 -0700

    [bcnotif] Bump version

--- packages/bcnotif.nix
+++ packages/bcnotif.nix
@@ -2,16 +2,16 @@ with import <nixpkgs> {};
 
 rustPlatform.buildRustPackage rec {
     name = "bcnotif-${version}";
-    version = "8ed652ae7cb9db8cc9e979bf8ba05033720ccfbd";
+    version = "6fc45a95f9086cd5854580a6c301b61c590a2023";
     
     src = fetchFromGitLab {
         owner = "Acizza";
         repo = "bcnotif";
         rev = "${version}";
-        sha256 = "0cq3gxrrx0xl5c33qfx70pj1sar65fsb0npp7w1yr8001vdzk9f4";
+        sha256 = "1m6m5mcv0mq0mwjkx90fyqlj8y3sdwnvjzizxlghz2xv1zbaxvpz";
     };
     
-    cargoSha256 = "1pmppxbdpnya5spgl0lxy6xjyspsxzqqwjw1z0ndkrgd2c0wwpwm";
+    cargoSha256 = "10b9v8ringz4b9w7gswdfcqq3w7xsbcvs8pyg2acg7snzmp6mvcv";
     
     nativeBuildInputs = [ pkgconfig ];
     buildInputs = [ dbus.dev openssl.dev ];
