04e61adf85d92a3aae1461ad99e13ca82f0e31f0
Acizza <jonathanmce@pm.me>
2018-12-02 22:17:44 -0800

    [overlay] dxvk: v0.92 -> v0.93

--- overlays/pkgs/dxvk/default.nix
+++ overlays/pkgs/dxvk/default.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-  version = "v0.92";
+  version = "v0.93";
 in
   multiStdenv.mkDerivation {
     name = "dxvk-${version}";
@@ -15,7 +15,7 @@ in
       owner = "doitsujin";
       repo = "dxvk";
       rev = "${version}";
-      sha256 = "03yr6m20vq7mbba6m5dgbqgc89xbqnlkndvwrbw7cb19kadjm1hl";
+      sha256 = "0f8qz2sz82ph1f3i4yzgjcrq0w2xljx23g6frh4gf35rkfmamrh8";
     };
 
     buildInputs = with pkgs; [ meson ninja glslang ] ++ [ winePackage ];
