82520c054b8f15d8e563be5ce2538979803fe595
Acizza <jgit@tuta.io>
2019-02-12 17:38:09 -0800

    [overlay] nixup: bump to 06666370

--- overlays/pkgs/nixup.nix
+++ overlays/pkgs/nixup.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
     name = "nixup-${version}";
-    version = "b07d08282de5dc44281eb6484d42f2d353a234f9";
+    version = "0666637013515de497be39e9589748eb893a8649";
     
     src = fetchFromGitLab {
         owner = "Acizza";
         repo = "nixup";
         rev = "${version}";
-        sha256 = "1wnvasfnxsc524h17vh3hbpn1bghl56rnrsdz78lpwpdb9jwhvpm";
+        sha256 = "1jyjrqlmv9znlyrpsysiddkrrghcxl0xd8dib45bbf8zgbwa2gws";
     };
     
     cargoSha256 = "0hzps9n91k4rzl7xjjipvh22h0ii432113ldcry9zl2n8a0gjsz7";
