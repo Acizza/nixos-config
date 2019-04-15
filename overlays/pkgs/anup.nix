4f4f25a74dfb49ec4e7821137e3ba077094bcbf2
Acizza <jgit@tuta.io>
2019-04-15 06:47:01 -0700

    [overlay] anup: manually install anup.sh script
    The cargo build script for this program should install the script automatically, but it no longer seems to have any effect.

--- overlays/pkgs/anup.nix
+++ overlays/pkgs/anup.nix
@@ -5,19 +5,23 @@ rustPlatform.buildRustPackage rec {
     version = "6062dfbef4d5c0f061b9f6e342acab54f34e089a";
     
     src = fetchFromGitLab {
-        owner = "Acizza";
-        repo = "anup";
-        rev = "${version}";
-        sha256 = "03l720ya9bqlww18x0ymswvrcbmpg0m8bx1ky13s3zfzynz0ss9a";
+      owner = "Acizza";
+      repo = "anup";
+      rev = "${version}";
+      sha256 = "03l720ya9bqlww18x0ymswvrcbmpg0m8bx1ky13s3zfzynz0ss9a";
     };
     
     cargoSha256 = "1k0y9d282pmkpg94mpy4bimqkxj02av0rp2i0z4mcx8mwnrkj3hz";
     
     nativeBuildInputs = with pkgs; [ buildPackages.stdenv.cc pkgconfig ];
     buildInputs = with pkgs; [ openssl.dev xdg_utils ];
+
+    postInstall = ''
+      cp anup.sh $out/bin/anup.sh
+    '';
     
     meta = with stdenv.lib; {
-        license = licenses.asl20;
-        platforms = platforms.linux;
+      license = licenses.asl20;
+      platforms = platforms.linux;
     };
 }
