5104dc48bbdd9e6e031873b8577866ec18c97234
Acizza <jgit@tuta.io>
2019-05-01 16:28:48 -0700

    [overlay] faudio: 19.04 -> 19.05

--- overlays/pkgs/faudio.nix
+++ overlays/pkgs/faudio.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchFromGitHub, cmake, SDL2, ffmpeg}:
 
 let
-  version = "19.04";
+  version = "19.05";
 in
   stdenv.mkDerivation {
     name = "faudio-${version}";
@@ -10,10 +10,12 @@ in
       owner = "FNA-XNA";
       repo = "FAudio";
       rev = "${version}";
-      sha256 = "00lqf8bjcwm4k8yky9jmqghkxijcm2lxspb9zyl1270yqmj05kiw";
+      sha256 = "1dja2ykixk1ycqda116cg9fy4qg364dqj88amfln0r9pnsj2kbxk";
     };
 
     buildInputs = [ cmake SDL2.dev ffmpeg ];
+
+    patches = [ ../patches/faudio_take_dir_for_sdl_includes.patch ];
 
     NIX_CFLAGS_COMPILE = "-I${SDL2.dev}/include/SDL2";
   }
