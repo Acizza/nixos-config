bb95e93130dc073fad243ec6466f2a068634b799
Acizza <jgit@tuta.io>
2019-05-01 16:27:32 -0700

    [overlay] bcnotif: adjust cargoSha256 for NixOS update

--- overlays/pkgs/bcnotif.nix
+++ overlays/pkgs/bcnotif.nix
@@ -11,7 +11,7 @@ rustPlatform.buildRustPackage rec {
         sha256 = "1v2qqx9wrfdn6zqgndwj1m86b9ab5iq970vmf7d21xkbkk9kf7bx";
     };
     
-    cargoSha256 = "1v3002skk0nfbs3z3a38ic8wprgy5dz47x344zwd8gp10iv6gkys";
+    cargoSha256 = "11z7v4n8ppsdljqbfid1v5rvp9imqg6dfvjrsq4dd1nbp1pp5nc3";
     
     nativeBuildInputs = with pkgs; [ pkgconfig ];
     buildInputs = with pkgs; [ dbus.dev openssl.dev ];
