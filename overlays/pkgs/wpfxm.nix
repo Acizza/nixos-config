2ff94e3890e0645164909c696ac5100152770fca
Acizza <jgit@tuta.io>
2019-05-27 05:45:35 -0700

    [overlay] wpfxm: bump to 9d70945c

--- overlays/pkgs/wpfxm.nix
+++ overlays/pkgs/wpfxm.nix
@@ -1,22 +1,22 @@
-{ rustPlatform, fetchFromGitLab, pkgs, stdenv }:
+{ rustPlatform, fetchFromGitLab, stdenv }:
 
 rustPlatform.buildRustPackage rec {
     name = "wpfxm-${version}";
-    version = "v0.1.0";
+    version = "9d70945c2d7f2eba8dc2fe1049ec7103e2835380";
     
     src = fetchFromGitLab {
-        owner = "Acizza";
-        repo = "wpfxm";
-        rev = "${version}";
-        sha256 = "17zzj6z6m13i1dzqwkqfya77xpql27bjyfr4ysm5366i6d86fyg4";
+      owner = "Acizza";
+      repo = "wpfxm";
+      rev = "${version}";
+      sha256 = "046bnhs3r49ly5k1pjf2ll4r0whz4k1y1675h6671nqjhrw0l79z";
     };
     
-    cargoSha256 = "01sgajv00qfjm45r5bhr9dllmdsx23i6zp3p5cmqpjrdl6yqzdf2";
+    cargoSha256 = "0f1lsmq4zv20l0m6bv1ip41z45qs7r74axx2rvl7nzbv4gjilask";
     
     buildInputs = [ stdenv.cc.cc ];
     
     meta = with stdenv.lib; {
-        license = licenses.asl20;
-        platforms = platforms.linux;
+      license = licenses.asl20;
+      platforms = platforms.linux;
     };
 }
