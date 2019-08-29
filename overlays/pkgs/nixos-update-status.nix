907b7f819390cb56f268abdedf92676c0393df21
Acizza <jgit@tuta.io>
2019-08-29 12:15:15 -0700

    [overlay] nixos-update-status: bump to c49c6aa9

--- overlays/pkgs/nixos-update-status.nix
+++ overlays/pkgs/nixos-update-status.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage rec {
     name = "nixos-update-status-${version}";
-    version = "1c4592e53d4b8a87f13f06427e3c5b2b5403aef9";
+    version = "c49c6aa9c427df45c8b93bffe41ce13f4d2f952d";
     
     src = fetchFromGitLab {
       owner = "Acizza";
       repo = "nixos-update-status";
       rev = "${version}";
-      sha256 = "0m99iz4ralq9jk9q55vkai3cln8h627r10szgp1s513d210fgcm9";
+      sha256 = "0vg4dr3cw7n5j5hpx240mb8v6bgq0di7pyyasrkrqwy25dla31cy";
     };
     
-    cargoSha256 = "05ki1lvxh6bcvi14m352sa7ilp4jqy46fl5mv97w81fx65wjbg76";
+    cargoSha256 = "1df4kacifzw1nc98m0m9p9rsv56pmpr0zfraa9l1f4v5yjzinr85";
     
     buildInputs = [ stdenv.cc.cc pkgconfig openssl.dev ];
     
