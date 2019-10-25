6a6d2953dfc069964fa092a5f286914e84c601e1
Acizza <jgit@tuta.io>
2019-10-24 17:53:35 -0700

    dxvk: v1.4 -> v1.4.3

--- overlays/pkgs/dxvk/default.nix
+++ overlays/pkgs/dxvk/default.nix
@@ -7,50 +7,48 @@
   wine,
 }:
 
-let
-  version = "v1.4";
-in
-  multiStdenv.mkDerivation {
-    name = "dxvk-${version}";
+multiStdenv.mkDerivation rec {
+  pname = "dxvk";
+  version = "v1.4.3";
 
-    src = fetchFromGitHub {
-      owner = "doitsujin";
-      repo = "dxvk";
-      rev = "${version}";
-      sha256 = "1rjr9i993bdhg36ldc9384flmxk9nsjq2ln34ykzcizh9qpicgj1";
-    };
+  src = fetchFromGitHub {
+    owner = "doitsujin";
+    repo = "dxvk";
+    rev = version;
+    sha256 = "1hh5mnpdv7wmd5zn3a69m0fgjrakfaznrs0ff0wv74bp81r6yw6y";
+  };
 
-    buildInputs = [ meson ninja glslang wine ];
+  buildInputs = [ meson ninja glslang wine ];
 
-    phases = "unpackPhase patchPhase buildPhase installPhase fixupPhase";
+  phases = "unpackPhase patchPhase buildPhase installPhase fixupPhase";
 
-    patches = [ ../../patches/dxvk_fix_setup_script_hang.patch ];
+  patches = [ ../../patches/dxvk_fix_setup_script_hang.patch ];
 
-    buildPhase =
-      let
-        builder = ./builder.sh;
-      in ''
-        source ${builder}
-        build_dxvk 64
-        build_dxvk 32
-      '';
-
-    installPhase = ''
-      cp setup_dxvk.sh $out/share/dxvk/setup_dxvk
-      chmod +x $out/share/dxvk/setup_dxvk
-
-      mkdir -p $out/bin/
-      ln -s $out/share/dxvk/setup_dxvk $out/bin/setup_dxvk
+  buildPhase =
+    let
+      builder = ./builder.sh;
+    in ''
+      source ${builder}
+      build_dxvk 64
+      build_dxvk 32
     '';
 
-    fixupPhase = ''
-      substituteInPlace $out/share/dxvk/setup_dxvk --replace \
-        "#!/bin/bash" \
-        "#!${stdenv.shell}"
-    '';
-    
-    meta = with stdenv.lib; {
-      platforms = platforms.linux;
-      licenses = [ licenses.zlib licenses.png ];
-    };
-  }
+  installPhase = ''
+    cp setup_dxvk.sh $out/share/dxvk/setup_dxvk
+    chmod +x $out/share/dxvk/setup_dxvk
+
+    mkdir -p $out/bin/
+    ln -s $out/share/dxvk/setup_dxvk $out/bin/setup_dxvk
+  '';
+
+  fixupPhase = ''
+    substituteInPlace $out/share/dxvk/setup_dxvk --replace \
+      "#!/bin/bash" \
+      "#!${stdenv.shell}"
+  '';
+  
+  meta = with stdenv.lib; {
+    platforms = platforms.linux;
+    licenses = [ licenses.zlib licenses.png ];
+  };
+}
