e904bd7d6ea001932a9f2c504559d61d47113da8
Acizza <jgit@tuta.io>
2020-01-21 14:35:36 -0800

    bcnotif: bump to 5b7bdec

--- overlays/pkgs/bcnotif.nix
+++ overlays/pkgs/bcnotif.nix
@@ -1,23 +1,23 @@
-{ rustPlatform, fetchFromGitLab, pkgs, stdenv }:
+{ rustPlatform, fetchFromGitHub, pkgs, stdenv }:
 
 rustPlatform.buildRustPackage rec {
     name = "bcnotif-${version}";
-    version = "b83a72b7d520485d23a3d3dc0f83b8f02cbd91a4";
+    version = "5b7bdecb8d9cd95df9b0654281255a116ef67e1c";
     
-    src = fetchFromGitLab {
+    src = fetchFromGitHub {
         owner = "Acizza";
         repo = "bcnotif";
         rev = "${version}";
-        sha256 = "1v2qqx9wrfdn6zqgndwj1m86b9ab5iq970vmf7d21xkbkk9kf7bx";
+        sha256 = "1k5a6n1r2b0rirr7yhzknldw6qmh6j13f5rkq4bdcggbhflznclk";
     };
     
-    cargoSha256 = "11z7v4n8ppsdljqbfid1v5rvp9imqg6dfvjrsq4dd1nbp1pp5nc3";
+    cargoSha256 = "1zj6llaxl6d4gib4h27ps850rg04j36sprgbs6mi7kdk90148x46";
     
     nativeBuildInputs = with pkgs; [ pkgconfig ];
-    buildInputs = with pkgs; [ dbus.dev openssl.dev ];
+    buildInputs = with pkgs; [ dbus.dev openssl.dev sqlite.dev ];
     
     meta = with stdenv.lib; {
-        license = licenses.asl20;
+        license = licenses.agpl3;
         platforms = platforms.linux;
     };
 }
