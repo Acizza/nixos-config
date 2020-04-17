68c657d10e5177841871a50f413bc65407e57298
Acizza <jgit@tuta.io>
2020-04-17 11:05:08 -0700

    linux: 5.5 -> 5.6

--- configuration.nix
+++ configuration.nix
@@ -20,16 +20,16 @@
     kernel.sysctl."fs.inotify.max_user_watches" = 524288;
     
     cleanTmpDir = true;
-    kernelPackages = pkgs.linuxPackages_5_5;
+    kernelPackages = pkgs.linuxPackages_5_6;
 
     # Add fsync patch for Wine
     kernelPatches =
       let
         fsync = rec {
-          name = "v5.5-fsync";
+          name = "v5.6-fsync";
           patch = pkgs.fetchpatch {
             name = name + ".patch";
-            url = "https://raw.githubusercontent.com/Tk-Glitch/PKGBUILDS/master/linux55-tkg/linux55-tkg-patches/0007-v5.5-fsync.patch";
+            url = "https://raw.githubusercontent.com/Frogging-Family/linux-tkg/9df993642a3a87f7a4027d2e03195359b1355158/linux56-tkg/linux56-tkg-patches/0007-v5.6-fsync.patch";
             sha256 = "15zgjjn3ighh2cfgj3904z9hdbdk69z58xfyjdlj5dfh094p0kv2";
           };
         };
