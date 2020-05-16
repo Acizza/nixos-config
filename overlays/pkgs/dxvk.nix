633ef3ea5ac40391ac5329a1d930b41927a5a9ea
Acizza <jgit@tuta.io>
2020-05-16 16:48:17 -0700

    dxvk: 1.6.1 -> 1.7

--- overlays/pkgs/dxvk.nix
+++ overlays/pkgs/dxvk.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "dxvk";
-  version = "1.6.1";
+  version = "1.7";
 
   src = fetchurl {
     url = "https://github.com/doitsujin/dxvk/releases/download/v${version}/dxvk-${version}.tar.gz";
-    sha256 = "0dli6pqlgqym8s92cp2h54z5am5dxb67pcr3myvwrn9y64sqgvyd";
+    sha256 = "18f7lj6b08abywidsq3s98kiwwn1jbbjzg7clm8bs93cj0wq5mv7";
   };
 
   phases = "unpackPhase installPhase fixupPhase";
