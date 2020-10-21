547f6d1f98bbaac294d144a717cb1aedcbcbfcfc
Acizza <jgit@tuta.io>
2020-10-20 22:25:12 -0700

    anup: Bump to c2abebd

--- overlays/pkgs/anup.nix
+++ overlays/pkgs/anup.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
     name = "anup-${version}";
-    version = "0a05243097f96cc8fb9246e36ea99439120acd09";
+    version = "c2abebd6f07d6009537efe1d2935c3aa7cfbf56a";
     
     src = fetchFromGitHub {
       owner = "Acizza";
       repo = "anup";
       rev = version;
-      sha256 = "18sav9nvc446d4w4k2mwi9vn17ai0na9ivxz1ysv01ay7bxv0q36";
+      sha256 = "TQJIaByQ5qsqnMcJXv/zXz19MTJIA7uZWl0liwS3E4U=";
     };
     
-    cargoSha256 = "1vcx4h2s6gyhyjsm18x8iqdpcimlv25j397z19rya758gc1rnzah";
+    cargoSha256 = "P2M0JfhYGnBNY310NVFP3K4KrG0Tv7XVomWQnXhFR2s=";
     
     buildInputs = [ stdenv.cc pkgconfig sqlite.dev xdg_utils ];
     
