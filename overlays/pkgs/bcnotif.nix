8e01c72005aa80759b4ee58a5b0b48cd8af826b5
Acizza <jgit@tuta.io>
2020-11-02 17:13:48 -0800

    bcnotif: Bump to 071f3f5

--- overlays/pkgs/bcnotif.nix
+++ overlays/pkgs/bcnotif.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
     name = "bcnotif-${version}";
-    version = "cd576941f6c3c6288e730afcc0a681d3d5f06abb";
+    version = "071f3f5cc1d4de7d7b4ad202316cd4bc098665f6";
     
     src = fetchFromGitHub {
         owner = "Acizza";
         repo = "bcnotif";
         rev = "${version}";
-        sha256 = "0kd48va6fxabd2251w5245i5ph2yysnbfypnjfd242ixqqis7fbw";
+        sha256 = "DPYi1zbqcS2/gD2wuhF673m6sRDWhtp4IDQ5dvJFZwE=";
     };
     
-    cargoSha256 = "18hfki0sm6209kq4fbfis6ykb2zmq4biyl3jy5pjmaif1cs2ibpk";
+    cargoSha256 = "TjBBLaEdj5D5JtX6+qrgN5TAe7Q/7Ejmm333Xzqdd7s=";
     
     nativeBuildInputs = with pkgs; [ pkgconfig ];
     buildInputs = with pkgs; [ dbus.dev sqlite.dev ];
