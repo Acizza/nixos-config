745253592da758f3367c694e16f13e5427d8e986
Acizza <jgit@tuta.io>
2021-02-08 20:11:55 -0800

    anup: Bump to 0.1.1

--- overlays/pkgs/anup.nix
+++ overlays/pkgs/anup.nix
@@ -1,21 +1,28 @@
-{ rustPlatform, fetchFromGitHub, stdenv, pkgconfig, sqlite, xdg_utils }:
+{ rustPlatform,
+  fetchFromGitHub,
+  stdenv,
+  lib,
+  pkgconfig,
+  sqlite,
+  xdg_utils
+}:
 
 rustPlatform.buildRustPackage rec {
     pname = "anup";
-    version = "63cbd60c5d764f1183956ce56a4c9919b2d3e77c";
+    version = "0.1.1";
     
     src = fetchFromGitHub {
       owner = "Acizza";
       repo = "anup";
       rev = version;
-      sha256 = "/MknZIWLilEoMdXGJMv0h9D+K5ZzPgyxqFQUGxA3bYc=";
+      sha256 = "oL7dCXKpZKqfwFPX7NAVJ9AJq4o2tSX9fadQOTC9ogY=";
     };
     
-    cargoSha256 = "FvpJJSRl0ZxYrwF4YFWAKior+j+Vtbg3PxV0ihie1DQ=";
+    cargoSha256 = "c36xugGBz8cSvEcGggPk9NvOcR1IzXoOOxwzwG8rLFs=";
     
     buildInputs = [ stdenv.cc pkgconfig sqlite.dev xdg_utils ];
     
-    meta = with stdenv.lib; {
+    meta = with lib; {
       license = licenses.agpl3;
       platforms = platforms.linux;
     };
