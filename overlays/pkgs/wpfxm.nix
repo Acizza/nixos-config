41aa5113082c76ce8319e58a506eba1cbc9920e9
Acizza <jgit@tuta.io>
2021-04-23 22:50:36 -0700

    wpfxm: Update cargoSha256

--- overlays/pkgs/wpfxm.nix
+++ overlays/pkgs/wpfxm.nix
@@ -1,17 +1,17 @@
-{ rustPlatform, fetchFromGitLab, stdenv }:
+{ rustPlatform, fetchFromGitHub, stdenv }:
 
 rustPlatform.buildRustPackage rec {
     name = "wpfxm-${version}";
     version = "9d70945c2d7f2eba8dc2fe1049ec7103e2835380";
     
-    src = fetchFromGitLab {
+    src = fetchFromGitHub {
       owner = "Acizza";
       repo = "wpfxm";
-      rev = "${version}";
+      rev = version;
       sha256 = "046bnhs3r49ly5k1pjf2ll4r0whz4k1y1675h6671nqjhrw0l79z";
     };
     
-    cargoSha256 = "12rkgcy43gq656am9mz72nrs6b31vz7cd8156xvi1cszg0nv4vya";
+    cargoSha256 = "hV1som1cJZmzDdmixavUdibGifETQStJe3tF8mn5sso=";
     
     buildInputs = [ stdenv.cc.cc ];
     
