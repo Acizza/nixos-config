8b05d2fc29944e1bfdfe137482f2c74cfdeccc83
Acizza <jgit@tuta.io>
2021-04-23 22:49:37 -0700

    anup: Bump to 8dc8213

--- overlays/pkgs/anup.nix
+++ overlays/pkgs/anup.nix
@@ -9,16 +9,16 @@
 
 rustPlatform.buildRustPackage rec {
     pname = "anup";
-    version = "0.1.1";
+    version = "8dc8213fee8ddec1b719b43069e24deeb9f655e1";
     
     src = fetchFromGitHub {
       owner = "Acizza";
       repo = "anup";
       rev = version;
-      sha256 = "oL7dCXKpZKqfwFPX7NAVJ9AJq4o2tSX9fadQOTC9ogY=";
+      sha256 = "vpqTILH6TurlXIXnG6L07XqXf2NAExVkq8XT4JBsrp4=";
     };
     
-    cargoSha256 = "c36xugGBz8cSvEcGggPk9NvOcR1IzXoOOxwzwG8rLFs=";
+    cargoSha256 = "NSaRFVQDzWLYYdoo52H4prOD/prWBFZMgNhp7XM1r54=";
     
     buildInputs = [ stdenv.cc pkgconfig sqlite.dev xdg_utils ];
     
